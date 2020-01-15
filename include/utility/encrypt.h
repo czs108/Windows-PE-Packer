8d36c189671c99bb5c72ef30177d58aceb26dfe0
Chenzs108 <chenzs108@outlook.com>
2020-01-15 21:21:13 +0800

    Feat: Add the encryption

---
+++ include/utility/encrypt.h
@@ -0,0 +1,24 @@
+/**
+ * @file encrypt.h
+ * @brief Encrypt the data.
+ * @author Chen Zhenshuo (chenzs108@outlook.com)
+ * @version 1.0
+ * @date 2020-01-13
+ * 
+ * @par GitHub
+ * https://github.com/czs108/
+ */
+
+#pragma once
+
+#include <windows.h>
+
+/**
+ * @brief Encrypt the data.
+ * 
+ * @param base  The base address of the data.
+ * @param size  The size of the data.
+ */
+void EncryptData(
+    BYTE *const base,
+    const DWORD size);
