7b8ad185bcb4e23fedf78b5fb22c1fa61b793674
Chenzs108 <chenzs108@outlook.com>
2020-10-13 23:11:52 +0800

    Chore: Add a `bin` directory for executable files

--- build.ps1
+++ build.ps1
@@ -1,5 +1,10 @@
+New-Variable -Name 'AppName' -Value 'PE-Packer' -Option Constant
+
+New-Item -Name 'bin' -ItemType 'Directory' -Force
 New-Item -Name 'build' -ItemType 'Directory' -Force
 Set-Location -Path '.\build'
 
 Start-Process -FilePath 'cmake' -ArgumentList '..', '-D CMAKE_C_COMPILER=gcc', '-G "MinGW Makefiles"' -NoNewWindow -Wait
 Start-Process -FilePath 'cmake' -ArgumentList '--build', '.' -NoNewWindow -Wait
+
+Move-Item -Path ".\bin\$AppName.exe" -Destination '..\bin' -Force
